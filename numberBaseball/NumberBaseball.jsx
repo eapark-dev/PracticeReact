e7e5e728fb4323d09feb96ad6f8090408886eac8
eapark-dev <eapark5908@gmail.com>
2021-04-15 22:12:03 +0900

    Revert "리액트 Hooks 방식"
    This reverts commit 4a709c5a097a3e7f9b4e68bfe7e7973ac080f879.

--- numberBaseball/NumberBaseball.jsx
+++ numberBaseball/NumberBaseball.jsx
@@ -1,5 +1,5 @@
 //파일을 쪼개는 경우 꼭 필요한 패키지 라이브러리를 가져와야한다.
-import React, {useState} from 'react';
+import React, { Component } from 'react';
 import Try from './Try';
 
 function getNumbers () { //숫자 네 개를 겹치지 않고 랜덤하게 뽑는 함수
@@ -12,72 +12,84 @@ function getNumbers () { //숫자 네 개를 겹치지 않고 랜덤하게 뽑�
     return array;
 }
 
-const NumberBaseball = () => {
-    const [result, setResult] = useState(''); 
-    const [value, setValue] = useState('');
-    const [answer, setAnswer] = useState(getNumbers());
-    const [tries, setTries] = useState([]);
-    
-    const onSubmitForm = (e) => {
+class NumberBaseball extends Component {
+    state = {
+        result: '',
+        value: '',
+        answer: getNumbers(), //ex [1,3,5,7]
+        tries:[], //리액트에서는 불변성 때문에 push를 사용하면 안된다.
+    };
+
+    onSubmitForm = (e) => {
         e.preventDefault();
-        if(value === answer.join('')){  //답을 제출하면 답을 비교
-            setResult('홈런!');
-            setTries((prevTries)=> {
-                return [...prevTries, {try : value, result: '홈런!' }] 
-            });
+        if(this.state.value === this.state.answer.join('')){  //답을 제출하면 답을 비교
+            this.setState({
+                result : '홈런!',
+                tries: [...this.state.tries, {try : this.state.value, result: '홈런!' }] //기존 배열을 복사해서 새로운 거 넣어주기.
+            })
             alert('게임을 다시 시작합니다.');
-            setValue('');
-            setAnswer(getNumbers());
-            setTries([]);
+            this.setState( {
+                value: '',
+                answer : getNumbers(),
+                tries: [],
+            });
         }else{ //답이 아닐 경우
-            const answerArry = value.split('').map( (v)=> parseInt(v));
+            const answerArry = this.state.value.split('').map( (v)=> parseInt(v));
             let strike =0;
             let ball = 0;
-            if(tries.length >= 9) { //10번 이상  틀렸을 때
-                setResult(`10번 넘게 틀려서 실패! 답은 ${answer.join(',')} 였습니다!`);
+            if(this.state.tries.length >= 9) { //10번 이상  틀렸을 때
+                this.setState( {
+                    result : `10번 넘게 틀려서 실패! 답은 ${this.state.answer.join(',')} 였습니다!`,
+                });
                 alert('게임을 다시 시작합니다.');
-                setValue('');
-                setAnswer(getNumbers());
-                setTries([]);
+                this.setState( {
+                    value: '',
+                    answer : getNumbers(),
+                    tries: [],
+                });
             }else { 
                 for (let i =0; i < 4; i += 1) {
-                    if(answerArry[i] === answer[i]) {
+                    if(answerArry[i] === this.state.answer[i]) {
                         strike += 1;
-                    } else if(answer.includes(answerArry[i])){
+                    } else if(this.state.answer.includes(answerArry[i])){
                         ball += 1;
                     }
                 }
-                setTries((prevTries)=> [...prevTries, {try: value, result: `${strike} 스트라이크, ${ball} 볼입니다.`}]);
-                setValue('');
+                this.setState( {
+                    tries: [...this.state.tries, {try: this.state.value, result: `${strike} 스트라이크, ${ball} 볼입니다.`}],
+                    value: '',
+                });
             }
-        }
+        };
     };
 
-    const onChangeInput = (e) => {
-        setValue(e.target.value);
+    onChangeInput = (e) => {
+        this.setState({ value: e.target.value });
     };
 
-    //map이란 리액트에서 반복문을 사용하는 함수
-    return (
-        <>
-            <h1>{result}</h1>
-            <form onSubmit={onSubmitForm}>
-                <input maxLength={4} value={value} onChange={onChangeInput}/>
-            </form>
-            <div>시도: {tries.length} </div>
-            <ul>
-                {tries.map( (v,i) => {
-                    return ( //key값에는 i 자체를 쓰면 안된다. 성능이 최적화 할 때 react에서 key를 보고 판단하는데 뭐를 바꿨는 지 알 수 없어서 사용하면 안됨.
-                        //key에 index를 넣으면 안됨
-                        <Try key={`${i + 1}차 시도`} tryInfo={v}/> //code가 길어질 경우 컴포넌트 단위로 분리시켜 가져오는 게 가독성에 좋다.
-                        //컴포넌트를 가져올 때 key, value값을 인식하지 못하기 때문에 value={} 와 index={} 로 가져가야한다.
-                    );
-                })}
-            </ul>
-        </>
-    );
+    render(){
+        //map이란 리액트에서 반복문을 사용하는 함수
+        return (
+            <>
+                <h1>{this.state.result}</h1>
+                <form onSubmit={this.onSubmitForm}>
+                    <input maxLength={4} value={this.state.value} onChange={this.onChangeInput}/>
+                </form>
+                <div>시도: {this.state.tries.length} </div>
+                <ul>
+                    {this.state.tries.map( (v,i) => {
+                        return ( //key값에는 i 자체를 쓰면 안된다. 성능이 최적화 할 때 react에서 key를 보고 판단하는데 뭐를 바꿨는 지 알 수 없어서 사용하면 안됨.
+                            //key에 index를 넣으면 안됨
+                            <Try key={`${i + 1}차 시도`} tryInfo={v}/> //code가 길어질 경우 컴포넌트 단위로 분리시켜 가져오는 게 가독성에 좋다.
+                            //컴포넌트를 가져올 때 key, value값을 인식하지 못하기 때문에 value={} 와 index={} 로 가져가야한다.
+                        );
+                    })}
+                </ul>
+            </>
+        );
+    }
 
-};
+}
 
 
 //다른 곳에서도 사용할 수 있게 모듈화 한다.
--- numberBaseball/Try.jsx
+++ numberBaseball/Try.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Try = ({ tryInfo }) => {
-    return (
-        <li>
-            <div>{tryInfo.try}</div>
-            <div>{tryInfo.result}</div>
-        </li>
-    )
-};
+class Try extends Component {
+    render() { //props가 생기면서 부모 자식관계가 생긴다. 부모 컴포넌트가 자식 컴포넌트한테 props를 준다.
+        return (
+            <li>
+                <div>{this.props.tryInfo.try}</div>
+                <div>{this.props.tryInfo.result}</div>
+            </li>
+        )
+    }
+}
 
 export default Try;
 

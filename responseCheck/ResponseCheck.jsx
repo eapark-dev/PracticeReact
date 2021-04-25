afb0bee9966aba422c53f1ac7675d7102ac5d12c
eapark-dev <eapark5908@gmail.com>
2021-04-25 14:16:41 +0900

    리액트 class 형식으로 만든 반응속도체크

--- responseCheck/ResponseCheck.jsx
+++ responseCheck/ResponseCheck.jsx
@@ -7,16 +7,50 @@ class ResponseCheck extends Component {
         result : [],
     };
 
+    //랜더링이 되지 않도록 하기 위해 state안이 아니라 바깥에 두어야함.
+    timeout;
+    startTime;
+    endTime;
+    //클래스 사용시 구조분해 필수
     onClickScreen = () => {
-
+        const { state, message, result } = this.state;
+        if(state === 'waiting') {
+            this.setState( {
+                state : 'ready',
+                message : '초록색이 되면 클릭하세요',
+            });
+            this.timeout = setTimeout( () => {
+                this.setState( {
+                    state : 'now',
+                    message : '지금 클릭',
+                });
+                this.startTime = new Date();
+            },  Math.floor( Math.random() * 1000) + 2000); //2~3초 랜덤
+        } else if (state === 'ready') {
+            clearTimeout(this.timeout); //timeout 초기화
+            this.setState( {
+                state : 'waiting' ,
+                message : '성급하시군요! 초록색이 된 후에 클릭하세요.',
+            });
+            
+        }else if( state === 'now') {
+            this.endTime = new Date();
+            this.setState( (prevState) =>  {
+                return {
+                    state : 'waiting' ,
+                    message : '클릭해서 시작하세요',
+                    result: [...prevState.result, this.endTime - this.startTime ],
+                };
+            });
+        }
     };
 
     renderAverage = () => {
         const {result} = this.state;
         return result.length === 0
-            ? null
-            : <div>평균시간 : {esult.reduce((a,b) => a + c) / result.length}ms</div>
-    }
+            ? null //태그가 없는 걸 의미함
+            : <div>평균시간 : {result.reduce((a,c) => a + c) / result.length}ms</div>
+    };
 
     render() { //jsx안에서는 for과 if를 쓸 수 없다.
         const  {state, message} = this.state;

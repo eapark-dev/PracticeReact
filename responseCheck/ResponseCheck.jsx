7c6736e4096d12ef90f22d98393c179565402a63
eapark-dev <eapark5908@gmail.com>
2021-04-25 15:42:50 +0900

    리액트 훅스로 만든 반응속도 체크 웹 게임

--- responseCheck/ResponseCheck.jsx
+++ responseCheck/ResponseCheck.jsx
@@ -1,72 +1,75 @@
-import React, { Component } from 'react';
+import React, { useState  , useRef}  from 'react';
 
-class ResponseCheck extends Component {
-    state = {
-        state : 'waiting',
-        message : '클릭해서 시작하세요',
-        result : [],
-    };
+const ResponseCheck = () => {
+    const [state, setState] = useState('waiting');
+    const [message, setMessage] = useState('클릭해서 시작하세요');
+    const [result, setResult] = useState([]);
+    //값이 바뀌어도 랜더링 시키고싶지 않은 값들은 useRef 사용해야함.
+    const timeOut = useRef(null);
+    const startTime = useRef();
+    const endTime = useRef();
 
-    //랜더링이 되지 않도록 하기 위해 state안이 아니라 바깥에 두어야함.
-    timeout;
-    startTime;
-    endTime;
-    //클래스 사용시 구조분해 필수
-    onClickScreen = () => {
-        const { state, message, result } = this.state;
+    const onClickScreen = () => {
         if(state === 'waiting') {
-            this.setState( {
-                state : 'ready',
-                message : '초록색이 되면 클릭하세요',
-            });
-            this.timeout = setTimeout( () => {
-                this.setState( {
-                    state : 'now',
-                    message : '지금 클릭',
-                });
-                this.startTime = new Date();
+            setState ('ready');
+            setMessage ('초록색이 되면 클릭하세요');
+            timeOut.current = setTimeout( () => {
+                setState('now');
+                setMessage('지금 클릭');
+                startTime.current = new Date();
             },  Math.floor( Math.random() * 1000) + 2000); //2~3초 랜덤
         } else if (state === 'ready') {
-            clearTimeout(this.timeout); //timeout 초기화
-            this.setState( {
-                state : 'waiting' ,
-                message : '성급하시군요! 초록색이 된 후에 클릭하세요.',
-            });
+            clearTimeout(timeOut.current); //timeout 초기화
+            setState ('waiting');
+            setMessage ('성급하시군요! 초록색이 된 후에 클릭하세요.');
             
         }else if( state === 'now') {
-            this.endTime = new Date();
-            this.setState( (prevState) =>  {
-                return {
-                    state : 'waiting' ,
-                    message : '클릭해서 시작하세요',
-                    result: [...prevState.result, this.endTime - this.startTime ],
-                };
+            endTime.current = new Date();
+            setState ('waiting');
+            setMessage ('클릭해서 시작하세요');
+            setResult((preveResult) => {
+                return[...preveResult, endTime.current - startTime.current]
             });
         }
     };
 
-    renderAverage = () => {
-        const {result} = this.state;
-        return result.length === 0
-            ? null //태그가 없는 걸 의미함
-            : <div>평균시간 : {result.reduce((a,c) => a + c) / result.length}ms</div>
+    const onReset = () => {
+        setResult([]);
     };
 
-    render() { //jsx안에서는 for과 if를 쓸 수 없다.
-        const  {state, message} = this.state;
-        return(
-            <>
-                <div
-                    id="screen"
-                    className={state} //css -> class
-                    onClick = {this.onClickScreen}
-                >
-                    {message}
-                </div>
-                {this.renderAverage()}
+    const renderAverage = () => {
+        return result.length === 0
+            ? null //태그가 없는 걸 의미함
+            : <>
+                <div>평균시간 : {result.reduce((a,c) => a + c) / result.length}ms</div>
+                <button onClick={onReset}>리셋</button>
             </>
-        );
-    }
+
+    };
+
+    return (
+        <>
+            <div
+                id="screen"
+                className={state} //css -> class
+                onClick = {onClickScreen}
+            >
+                {message}
+            </div>
+            {/* 함수안에 if문 쓰는 방식 */}
+            {/* {(() => {
+                if(result.length === 0) {
+                    return null;
+                }else{
+                    return <>
+                        <div>평균시간 : {result.reduce((a,c) => a + c) / result.length}ms</div>
+                        <button onClick={onReset}>리셋</button>
+                    </>
+                }
+            })()} */}
+            {renderAverage()}
+        </>
+    );
 }
 
 export default ResponseCheck;

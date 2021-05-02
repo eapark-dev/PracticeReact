80f8d7e38899804d8a9566f8ec4e82d602bc3756
eapark-dev <eapark5908@gmail.com>
2021-05-02 14:42:40 +0900

    로또 추첨기 훅스 버전

--- RandomLotto/Lotto.jsx
+++ RandomLotto/Lotto.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { useState , useRef, useEffect , useMemo , useCallback} from 'react';
 import Ball from './Ball';
 
 function getWinNumbers() {
@@ -13,75 +13,63 @@ function getWinNumbers() {
     return [...winNumbers, bonusNumber];
 }
 
-class Lotto extends Component {
-    state = {
-        winNumbers: getWinNumbers(), // 당첨 숫자들
-        winBalls : [], //앞에 6개의 공
-        bnous : null, // 보너스 공
-        redo: false,
-    };
+//useCallback은 함수 자체를 기억하는 것
+//useMemo는 리턴 값을 기억하고 있는 것
 
-    timeouts = [];
+//hooks에서는 순서가 매우 중요하다.
+const Lotto = () => {
+    const LottoNumbers = useMemo( () => getWinNumbers(), []); //리턴값을 기억해 한번만 호출시키게 한다.
+    const [winNumbers, setWinNumbers] = useState(LottoNumbers);
+    const [winBalls, setWinBalls] = useState([]);
+    const [bonus , setBonus] = useState(null);
+    const [redo, setRedo] = useState(false);
+    const timeOuts = useRef([]); 
 
-    runTimeounts = () => {
-        const {winNumbers} = this.state
+    //inputs 자리가 빈 배열이면 componentDidMount() 와 같음
+    useEffect( () => {
+        console.log('useEffect');
         for (let i =0; i<winNumbers.length -1; i++) {
-            this.timeouts[i] = setTimeout(() => {
-                this.setState((prevState) => {
-                    return {
-                    winBalls: [...prevState.winBalls, winNumbers[i]],
-                    };
-                });
+            timeOuts.current[i] = setTimeout(() => {
+                setWinBalls((prevBalls) => [...prevBalls, winNumbers[i]]);
             }, (i + 1) * 1000);
         }
-        this.timeouts[6] = setTimeout(()=>{
-            this.setState( {
-                bnous : winNumbers[6],
-                redo : true  , //한번 더 버튼이 보너스 공까지 나왔을 때 다시 뽑을 수 있도록 표시되기 위해 true로 해줌
+        timeOuts.current[6] = setTimeout(()=>{
+            setBonus(winNumbers[6]);
+            setRedo(true);
+        } , 7000);
+        return() => {
+            timeOuts.current.forEach((v) => {
+                clearTimeout(v);
             });
-        } , 7000)
-    };
+        };
+    }, [timeOuts.current]);
+    //배열에 요소가 있으면 componentDidMount랑 componentDidUpdate 둘 다 수행
 
-    componentDidMount() {
-        this.runTimeounts();
-    }
 
-    componentWillUnmount() {
-        this.timeouts.forEach( (v) => {
-            clearTimeout(v);
-        });
-    }
+    //함수 자체를 기억하고 있어 여러번 실행시켜주지 않는다.
+    const onClickRedo = useCallback(() => {
+        //useCallback에서 사용하는 te는 항상 inputs에 넣어주어야한다.
+        //두번째 요소가 바뀌었을 때 실행
+        setWinNumbers(getWinNumbers());
+        setWinBalls([]);
+        setBonus(null);
+        setRedo(false);
+        timeOuts.current = [];
+    }, [winNumbers]);
 
-    componentDidUpdate(preveProps, prevState) {
-        if(this.state.winBalls.length === 0) {
-            this.runTimeounts();
-        }
-    }
-
-    onClickRedo = () => {
-        this.setState({
-            winNumbers: getWinNumbers(), // 당첨 숫자들
-            winBalls : [], //앞에 6개의 공
-            bnous : null, // 보너스 공
-            redo: false,
-        });
-        this.timeouts = [];
-    };
-
-    render() {
-        const {winBalls, bnous, redo} = this.state;
-        return (
-            <> 
-                <div>당첨 숫자</div>
-                <div id="결과창">
-                    {winBalls.map((v) => <Ball key={v} number={v} />)}
-                </div>
-                <div>보너스!</div>
-                {bnous && <Ball number = {bnous} />}
-                {redo && <button onClick={this.onClickRedo} >한번 더!</button>}
-            </>
-        );
-    }
+    return (
+        <> 
+            <div>당첨 숫자</div>
+            <div id="결과창">
+                {winBalls.map((v) => <Ball key={v} number={v} />)}
+            </div>
+            <div>보너스!</div>
+            {bonus && <Ball number = {bonus} />}
+            {redo && <button onClick={onClickRedo} >한번 더!</button>}
+        </>
+    );
 }
+
+
 
 export default Lotto;

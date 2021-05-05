4ec942e0fe2d082009bb55f2d3970d8da219f576
eapark-dev <eapark5908@gmail.com>
2021-05-05 21:00:27 +0900

    zerocho 웹 게임 틱택톡 - reducer, action, dispatch의 관계

--- TickTacToe/TickTacToe.jsx
+++ TickTacToe/TickTacToe.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useReducer} from 'react';
+import React, {useState, useReducer, useCallback} from 'react';
 import Table from './Table';
 
 const initialState = {
@@ -7,16 +7,31 @@ const initialState = {
     tableData : [['','',''],['','',''],['','','']],
 }
 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_WINNER' :
+            //state.wiiner = action.wiiner; 이런식으로 사용하면 안됨.
+            return {
+                ...state, //기존 state가 얕게 복사됨. 불변성
+                winner : action.winner,
+            }
+    }
+};
+
 const TickTacToe = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
+
+    const onclickTable = useCallback( () => {
+        dispatch({ type: 'SET_WINNER' , winner: '0'}) ;
+    }, []);
 
     // const [winner, setWinner] = useState('');
     // const [turn, setTurn] = useState('0');
     // const [tableData , setTableData] = useState([['','',''],['','',''],['','','']]);
     return (
         <>
-            <Table />
-            {winner && <div>{winnder}님의 승리</div>}
+            <Table onClick={onclickTable} tableData={state.tableData}/>
+            {state.winner && <div>{state.winner}님의 승리</div>}
         </>
     );
 }
